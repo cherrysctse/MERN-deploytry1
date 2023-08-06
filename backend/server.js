cafbeb5abcadffaeb5736ac136cc2793f15be4ae
cherrysctse <133928214+cherrysctse@users.noreply.github.com>
2023-08-06 12:30:31 -0700

    update server mongoDB link

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 // const uri = process.env.ATLAS_URI;
-//const uri = "mongodb://127.0.0.1:27017/activitiescollections";
-const uri = "mongodb+srv://cherry703:q4073rqyOSNSVnGO@cluster0.ac1mmob.mongodb.net/activitiescollections";
+const uri = "mongodb://127.0.0.1:27017/activitiescollections";
+//const uri = "mongodb+srv://cherry703:q4073rqyOSNSVnGO@cluster0.ac1mmob.mongodb.net/activitiescollections";
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 
