5da6fa8159d13244e22cf0587b7b14a6d977deb1
cherrysctse <133928214+cherrysctse@users.noreply.github.com>
2023-08-06 16:41:23 -0700

    update model

--- backend/models/activitylist.model.js
+++ backend/models/activitylist.model.js
@@ -8,6 +8,6 @@ const activitySchema = new Schema({
   activity: { type: String, required: true },
 });
 
-// This Activity creates the collection called activitimodels
+// This Activitry creates the collection called activitimodels
 const Activitymodel = mongoose.model("Activitymodel", activitySchema);
 module.exports = Activitymodel;
